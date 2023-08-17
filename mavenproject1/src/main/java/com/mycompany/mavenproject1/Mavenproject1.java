37eaf81d1f2eb6968b31b37dc97fa6ee13152fc4
INFABLE-7 <1460446@senati.pe>
2023-08-17 11:25:31 -0500

    creamos variables y le puse una funcion de  suma + o resta -

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -15,12 +15,17 @@ public class Mavenproject1 {
     public static void main(String[] args) {
         
         System.out.println("#########OPERACIONES MATEMATICAS#######");
+        System.out.println("calculamos la suma y la resta de los numero que ingrese: ");
+                
         
        Scanner sc = new Scanner(System.in);
         System.out.println("INGRESE EL PRIMER NUMERO");
         int num1=sc.nextInt();
         System.out.println("INGRSE EL SEGUNDO NUMERO");
         int num2 = sc.nextInt();
+        int suma=(num1+num2);
+        int resta=(num1+num2);
+        System.out.println("el resultado de la suma de:"+num1+"mas"+num2+"es de:"+suma);
         
         
         
