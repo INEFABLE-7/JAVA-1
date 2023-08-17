10fd0e5d5d8cec923677b76ec540b7bfbac085a8
INFABLE-7 <1460446@senati.pe>
2023-08-17 11:28:39 -0500

    imprimir texto  y tambien el resultado de la funcion antriormente agregada

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -26,7 +26,7 @@ public class Mavenproject1 {
         int suma=(num1+num2);
         int resta=(num1+num2);
         System.out.println("el resultado de la suma de:"+num1+"mas"+num2+"es de:"+suma);
-        
+        System.out.println("el resultado de la resta de:"+num1+"mas"+num2+"es de:"+resta);
         
         
         
